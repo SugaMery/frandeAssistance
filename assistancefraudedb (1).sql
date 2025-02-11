d0c049285ee9a94f908b3270be129292beb7841d
Maryam Bril <bmaryam@ept.sn>
2025-02-11 12:56:29 +0100

    tooo

--- assistancefraudedb (1).sql
+++ assistancefraudedb (1).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 31 jan. 2025 à 18:47
+-- Généré le : mar. 11 fév. 2025 à 12:56
 -- Version du serveur : 10.4.32-MariaDB
--- Version de PHP : 8.0.30
+-- Version de PHP : 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,8 +33,21 @@ CREATE TABLE `categories` (
   `slug` varchar(100) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `media_id` int(11) DEFAULT NULL
+  `media_id` int(11) DEFAULT NULL,
+  `description` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `slug`, `created_at`, `updated_at`, `media_id`, `description`) VALUES
+(23, 'Applications', 'applications', '2025-02-04 10:32:11', '2025-02-04 16:21:30', 23, 'Plateforme pour le développement et la gestion d\'applications mobiles et desktop.'),
+(24, 'Sites Web', 'sites-web', '2025-02-04 10:34:12', '2025-02-04 16:21:30', 24, 'Création, hébergement et gestion de sites web professionnels et personnels.'),
+(25, 'Téléphones', 'telephones', '2025-02-04 10:38:21', '2025-02-04 16:21:30', 25, 'Vente et réparation de téléphones mobiles et accessoires.'),
+(26, 'Entreprises', 'entreprises', '2025-02-04 10:39:35', '2025-02-04 16:21:30', 26, 'Services et conseils pour les entreprises et entrepreneurs.'),
+(27, 'Cartes Bancaires', 'cartes-bancaires', '2025-02-04 10:41:10', '2025-02-04 16:21:30', 27, 'Solutions et informations sur les cartes bancaires et services financiers.'),
+(28, 'Réseaux Sociaux', 'reseaux-sociaux', '2025-02-04 10:42:26', '2025-02-04 16:21:30', 28, 'Gestion et optimisation des comptes sur les réseaux sociaux.');
 
 -- --------------------------------------------------------
 
@@ -65,6 +78,112 @@ CREATE TABLE `cities` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `cities`
+--
+
+INSERT INTO `cities` (`id`, `name`, `postal_code`, `country`, `created_at`, `updated_at`) VALUES
+(1, 'Paris', '75000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(2, 'Marseille', '13000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(3, 'Lyon', '69000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(4, 'Toulouse', '31000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(5, 'Nice', '06000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(6, 'Nantes', '44000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(7, 'Strasbourg', '67000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(8, 'Montpellier', '34000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(9, 'Bordeaux', '33000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(10, 'Lille', '59000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(11, 'Rennes', '35000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(12, 'Reims', '51100', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(13, 'Le Havre', '76600', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(14, 'Saint-Étienne', '42000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(15, 'Toulon', '83000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(16, 'Grenoble', '38000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(17, 'Dijon', '21000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(18, 'Angers', '49000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(19, 'Nîmes', '30000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(20, 'Villeurbanne', '69100', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(21, 'Clermont-Ferrand', '63000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(22, 'Le Mans', '72000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(23, 'Aix-en-Provence', '13090', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(24, 'Brest', '29200', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(25, 'Tours', '37000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(26, 'Amiens', '80000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(27, 'Limoges', '87000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(28, 'Annecy', '74000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(29, 'Perpignan', '66000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(30, 'Besançon', '25000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(31, 'Metz', '57000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(32, 'Orléans', '45000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(33, 'Boulogne-Billancourt', '92100', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(34, 'Mulhouse', '68100', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(35, 'Rouen', '76000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(36, 'Caen', '14000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(37, 'Nancy', '54000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(38, 'Argenteuil', '95100', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(39, 'Saint-Denis', '93200', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(40, 'Roubaix', '59100', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(41, 'Avignon', '84000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(42, 'Nanterre', '92000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(43, 'Poitiers', '86000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(44, 'Créteil', '94000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(45, 'Dunkerque', '59140', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(46, 'Versailles', '78000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(47, 'Pau', '64000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(48, 'Colombes', '92700', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(49, 'La Rochelle', '17000', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(50, 'Saint-Maur-des-Fossés', '94100', 'France', '2025-02-05 08:04:46', '2025-02-05 08:04:46'),
+(51, 'Champigny-sur-Marne', '94500', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(52, 'Aubervilliers', '93300', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(53, 'Béziers', '34500', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(54, 'La Seyne-sur-Mer', '83500', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(55, 'Cannes', '06400', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(56, 'Antibes', '06600', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(57, 'Calais', '62100', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(58, 'Saint-Nazaire', '44600', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(59, 'Drancy', '93700', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(60, 'Mérignac', '33700', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(61, 'Colmar', '68000', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(62, 'Ajaccio', '20000', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(63, 'Bourges', '18000', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(64, 'Issy-les-Moulineaux', '92130', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(65, 'Levallois-Perret', '92300', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(66, 'Valence', '26000', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(67, 'Quimper', '29000', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(68, 'Noisy-le-Grand', '93160', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(69, 'Villeneuve-d’Ascq', '59650', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(70, 'Vénissieux', '69200', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(71, 'Cergy', '95000', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(72, 'Pessac', '33600', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(73, 'Troyes', '10000', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(74, 'Rueil-Malmaison', '92500', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(75, 'Antony', '92160', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(76, 'La Roche-sur-Yon', '85000', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(77, 'Lorient', '56100', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(78, 'Chambéry', '73000', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(79, 'Niort', '79000', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(80, 'Sarcelles', '95200', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(81, 'Évreux', '27000', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(82, 'Saint-Quentin', '02100', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(83, 'Vannes', '56000', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(84, 'Cholet', '49300', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(85, 'Bondy', '93140', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(86, 'Saint-Ouen-sur-Seine', '93400', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(87, 'Fréjus', '83600', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(88, 'Clamart', '92140', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(89, 'Meaux', '77100', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(90, 'Narbonne', '11100', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(91, 'Bastia', '20200', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(92, 'Chelles', '77500', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(93, 'Castres', '81100', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(94, 'Montélimar', '26200', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(95, 'Bagneux', '92220', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(96, 'Suresnes', '92150', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(97, 'Le Blanc-Mesnil', '93150', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(98, 'Massy', '91300', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(99, 'Albi', '81000', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10'),
+(100, 'Grasse', '06130', 'France', '2025-02-05 08:05:10', '2025-02-05 08:05:10');
 
 -- --------------------------------------------------------
 
@@ -171,6 +290,20 @@ CREATE TABLE `login_history` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Déchargement des données de la table `login_history`
+--
+
+INSERT INTO `login_history` (`id`, `user_id`, `ip_address`, `user_agent`, `created_at`) VALUES
+(1, 5, '102.50.245.141', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36', '2025-02-10 13:52:17'),
+(2, 5, '102.50.245.141', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36', '2025-02-10 13:54:14'),
+(3, 5, '102.50.245.141', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36', '2025-02-10 14:02:52'),
+(4, 5, '102.50.245.141', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36', '2025-02-10 14:47:24'),
+(5, 5, '102.50.245.141', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36', '2025-02-10 16:16:34'),
+(6, 5, '160.176.136.180', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 OPR/116.0.0.0', '2025-02-10 19:58:40'),
+(7, 5, '102.50.245.141', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 OPR/116.0.0.0', '2025-02-11 09:10:40'),
+(8, 5, '102.50.245.141', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 OPR/116.0.0.0', '2025-02-11 09:17:21');
+
 -- --------------------------------------------------------
 
 --
@@ -179,11 +312,26 @@ CREATE TABLE `login_history` (
 
 CREATE TABLE `media` (
   `id` int(11) NOT NULL,
+  `report_id` int(11) DEFAULT NULL,
   `file_path` varchar(255) NOT NULL,
   `file_type` varchar(50) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `media`
+--
+
+INSERT INTO `media` (`id`, `report_id`, `file_path`, `file_type`, `created_at`, `updated_at`) VALUES
+(23, NULL, 'http://localhost:3000/uploads/1738665131207-securite (1).png', 'image/png', '2025-02-04 10:32:11', '2025-02-04 10:32:11'),
+(24, NULL, 'http://localhost:3000/uploads/1738665252773-world-wide-web.png', 'image/png', '2025-02-04 10:34:12', '2025-02-04 10:34:12'),
+(25, NULL, 'http://localhost:3000/uploads/1738665501484-telephoner (1).png', 'image/png', '2025-02-04 10:38:21', '2025-02-04 10:38:21'),
+(26, NULL, 'http://localhost:3000/uploads/1738665575253-mon-entreprise.png', 'image/png', '2025-02-04 10:39:35', '2025-02-04 10:39:35'),
+(27, NULL, 'http://localhost:3000/uploads/1738665670875-carte-bancaire.png', 'image/png', '2025-02-04 10:41:10', '2025-02-04 10:41:10'),
+(28, NULL, 'http://localhost:3000/uploads/1738665746228-reseaux-sociaux.png', 'image/png', '2025-02-04 10:42:26', '2025-02-04 10:42:26'),
+(39, 21, 'http://localhost:3000/uploads/1739219861489-Design_sans_titre__3_-removebg-preview.png', 'image/png', '2025-02-10 20:37:41', '2025-02-10 20:37:41'),
+(40, 21, 'http://localhost:3000/uploads/1739219861490-bg.png', 'image/png', '2025-02-10 20:37:41', '2025-02-10 20:37:41');
 
 -- --------------------------------------------------------
 
@@ -286,6 +434,13 @@ CREATE TABLE `reports` (
   `media_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Déchargement des données de la table `reports`
+--
+
+INSERT INTO `reports` (`id`, `user_id`, `title`, `description`, `category_id`, `city_id`, `status`, `image_path`, `allow_image_upload`, `created_at`, `updated_at`, `media_id`) VALUES
+(21, 5, 'titreeeeeeeeeeeefile.name', 'file.namefile.namefile.namefile.namefile.name', 23, 1, 'pending', NULL, 1, '2025-02-10 20:37:41', '2025-02-10 20:37:41', NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -330,6 +485,13 @@ CREATE TABLE `roles` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`, `slug`, `created_at`, `updated_at`) VALUES
+(1, 'Super Admin', 'suadmin', '2025-02-03 16:13:21', '2025-02-03 16:13:21');
 
 -- --------------------------------------------------------
 
@@ -405,6 +567,14 @@ CREATE TABLE `users` (
   `is_active` tinyint(1) DEFAULT 1,
   `deleted_reason_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `uuid`, `username`, `email`, `password`, `first_name`, `last_name`, `phone_number`, `address`, `postal_code`, `city`, `role_id`, `created_at`, `updated_at`, `deleted_at`, `is_active`, `deleted_reason_id`) VALUES
+(3, 'afa6f074-e24a-11ef-9ee1-f854f664ad2e', 'testuser', 'testuser@example.com', '$2b$10$zq6Tnvwgr4ZSBxdbiEAH1uXzzXQlvyJXXV9.GsAONQPBd4iEEcKWO', 'Test', 'User', NULL, NULL, NULL, NULL, 1, '2025-02-03 16:19:47', '2025-02-03 16:19:47', NULL, 1, NULL),
+(5, '7a2e77b1-e255-11ef-9ee1-f854f664ad2e', 'bmatryam@ept.sn', 'john.doe@example.com', '$2b$10$q.UD7xLRQd44EOqX.2FQyu/pbQC1k5BNGfq3ouIKk2144jNKhlO6a', 'Mariam', 'Bril', '0661327755', 'Marrakech', '4000', 'Marrakech', 1, '2025-02-03 17:37:02', '2025-02-03 17:37:02', NULL, 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -615,7 +785,7 @@ ALTER TABLE `user_statistics`
 -- AUTO_INCREMENT pour la table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT pour la table `chatbot_messages`
@@ -627,7 +797,7 @@ ALTER TABLE `chatbot_messages`
 -- AUTO_INCREMENT pour la table `cities`
 --
 ALTER TABLE `cities`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101;
 
 --
 -- AUTO_INCREMENT pour la table `comments`
@@ -669,13 +839,13 @@ ALTER TABLE `invoices`
 -- AUTO_INCREMENT pour la table `login_history`
 --
 ALTER TABLE `login_history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT pour la table `media`
 --
 ALTER TABLE `media`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT pour la table `notifications`
@@ -711,7 +881,7 @@ ALTER TABLE `plans`
 -- AUTO_INCREMENT pour la table `reports`
 --
 ALTER TABLE `reports`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT pour la table `report_statistics`
@@ -729,7 +899,7 @@ ALTER TABLE `reviews`
 -- AUTO_INCREMENT pour la table `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `secondary_accounts`
@@ -753,7 +923,7 @@ ALTER TABLE `suspicious_ips`
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `user_statistics`
